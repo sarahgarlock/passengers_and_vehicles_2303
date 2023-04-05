03095d19b5b89b4b44bd2fddcf889d681199cc95
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-05 09:42:28 -0600

    Add passengers

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -3,14 +3,29 @@ require 'pry'
 class Vehicle
   attr_reader :year,
               :make,
-              :model
+              :model,
+              :speeding,
+              :passengers
+
 
   def initialize(year, make, model)
     @year = year
     @make = make
     @model = model
+    @speeding = false
+    @passengers = []
 
   end
 
+  def speed
+    @speeding = true
+  end
 
+  def speeding?
+    @speeding
+  end
+
+  def add_passenger(passenger)
+    @passengers << passenger
+  end
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -25,5 +25,47 @@ RSpec.describe Vehicle do
 
       expect(vehicle.speeding?).to be false
     end
+
+    it 'can be speeding when called' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      vehicle.speed
+      
+      expect(vehicle.speeding?). to be true
+    end
+  end
+
+  describe '#passengers' do
+    it 'starts with 0 passengers' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+      expect(vehicle.passengers).to eq([])
+    end
+
+    it 'can add passengers' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    end
+
+    it 'adds number of adult passengers' do
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20}) 
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+
+      expect(vehicle.num_adults).to eq 2
+    end
   end
 end
